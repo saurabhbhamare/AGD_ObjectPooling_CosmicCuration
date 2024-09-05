f8e80688cccca9f91c99b008629e6908ae25fa8a
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-09-05 16:16:11 +0530

    Generic ObjectPool Implementation

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+        public T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.Item;
+                }
+            }
+            return CreateItem();
+        }
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException("not impl CreateItem in child class");
+        }
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool isUsed;
+        }
+    }
+
+}
